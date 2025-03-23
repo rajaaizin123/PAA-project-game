a80931715940d3c1e52adf2cb3b7fc9ba5f65957
Cahyadi240105 <cahyadiprasetyo659@gmail.com>
2025-03-23 19:42:49 +0700

    Again

--- src/main.c
+++ src/main.c
@@ -6,9 +6,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "../include/resource_dir.h"	// utility header for SearchAndSetResourceDir
+#include "../include/resource_dir.h" // utility header for SearchAndSetResourceDir
 
-#undef RAYGUI_IMPLEMENTATION            // Avoid including raygui implementation again
+#undef RAYGUI_IMPLEMENTATION // Avoid including raygui implementation again
 
 #define GUI_WINDOW_FILE_DIALOG_IMPLEMENTATION
 #include "gui_window_file_dialog.h"
@@ -21,7 +21,7 @@ typedef enum
 
 void resizeImage();
 
-int main ()
+int main()
 {
 	// Window Inilisiation
 	const int screenWidth = 1100;
@@ -40,12 +40,12 @@ int main ()
 	GameState currentScreen = MENU_STATE;
 
 	// “Start” button
-	//Rectangle startButton = {screenWidth / 2 - 100, screenHeight / 2 - 30, 200, 60};
+	// Rectangle startButton = {screenWidth / 2 - 100, screenHeight / 2 - 30, 200, 60};
 
 	// dirselector (handle upload png)
 	GuiWindowFileDialogState fileDialogState = InitGuiWindowFileDialog(GetWorkingDirectory());
-	char namaFileGambar[512] = { 0 };
-	Texture map_texture = { 0 };
+	char namaFileGambar[512] = {0};
+	Texture map_texture = {0};
 	Image map;
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
@@ -54,72 +54,95 @@ int main ()
 	// game loop
 	while (!WindowShouldClose())
 	{
+		//  proses drawing
 		BeginDrawing();
+
+		// Setup the back buffer for drawing (clear color and depth buffers)
 		ClearBackground(BLACK);
 
 		if (currentScreen == MENU_STATE)
 		{
-			// Tombol Play the Game
 			if (GuiButton((Rectangle){screenWidth / 2 - 50, screenHeight / 2 - 20, 100, 40}, "PLAY THE GAME"))
 			{
 				currentScreen = GAME_STATE;
-				startgame = false; // Reset status game jika kembali ke menu
 			}
 		}
 		else if (currentScreen == GAME_STATE)
 		{
+			if (fileDialogState.SelectFilePressed)
+			{
+				// Load image file (if supported extension)
+				if (IsFileExtension(fileDialogState.fileNameText, ".png"))
+				{
+					// gabung direktori dan nama file
+					strcpy(namaFileGambar, TextFormat("%s" PATH_SEPERATOR "%s", fileDialogState.dirPathText, fileDialogState.fileNameText));
+
+					// loading message
+					DrawText("Loading image...", screenWidth / 2 - 80, screenHeight / 2, 20, BLUE);
+					EndDrawing();
+
+					// Load image
+					map = LoadImage(namaFileGambar);
+					if (map.data == NULL)
+					{
+						printf("Gagal load gambar.\n");
+					}
+					else
+					{
+						UnloadTexture(map_texture);
+						map_texture = LoadTextureFromImage(map);
+					}
+				}
+
+				fileDialogState.SelectFilePressed = false;
+			}
+
+			DrawTexture(map_texture, GetScreenWidth() / 2 - map_texture.width / 2, GetScreenHeight() / 2 - map_texture.height / 2 + 10, WHITE);
+			DrawRectangleLines(GetScreenWidth() / 2 - map_texture.width / 2, GetScreenHeight() / 2 - map_texture.height / 2 + 10, map_texture.width, map_texture.height, BLACK);
+
+			// DrawText(namaFileGambar, 208, GetScreenHeight() - 20, 10, GRAY);
+
 			if (fileDialogState.windowActive)
 				GuiLock();
 
-			// Tombol Pilih Map
 			if (GuiButton((Rectangle){20, 20, 140, 30}, GuiIconText(ICON_FILE_OPEN, "Pilih Map")))
 				fileDialogState.windowActive = true;
-
-			// Tombol Start Game (di sebelah kanan tombol Pilih Map)
-			if (!startgame)
+			// Gambar Tombol "Start Game" di tengah layar
+			if (GuiButton((Rectangle){screenWidth / 2 - 70, screenHeight / 2 - 20, 140, 40}, "Start Game"))
 			{
-				if (GuiButton((Rectangle){180, 20, 140, 30}, "Start Game"))
-				{
-					startgame = true;
-				}
+				startgame = true;
+			}
+
+			// Jika tombol ditekan, tampilkan pesan
+			if (startgame)
+			{
+				DrawText("Game Dimulai!", screenWidth / 2 - 50, screenHeight / 2 + 50, 20, RED);
 			}
 
 			GuiUnlock();
-
-			// Tampilkan gambar map jika ada
-			if (map_texture.id > 0)
-			{
-				DrawTexture(map_texture, GetScreenWidth() / 2 - map_texture.width / 2, GetScreenHeight() / 2 - map_texture.height / 2 + 10, WHITE);
-			}
-
-			// Menggambar karakter kurir jika game sudah dimulai
-			if (startgame)
-			{
-				// Kontrol pergerakan kurir
-				if (IsKeyDown(KEY_RIGHT) && KurirPos.x < screenWidth - 20)
-					KurirPos.x += 5;
-				if (IsKeyDown(KEY_LEFT) && KurirPos.x > 20)
-					KurirPos.x -= 5;
-				if (IsKeyDown(KEY_UP) && KurirPos.y > 20)
-					KurirPos.y -= 5;
-				if (IsKeyDown(KEY_DOWN) && KurirPos.y < screenHeight - 20)
-					KurirPos.y += 5;
-
-				// Menggambar kurir (segitiga)
-				Vector2 v1 = {KurirPos.x, KurirPos.y - 20};
-				Vector2 v2 = {KurirPos.x - 20, KurirPos.y + 20};
-				Vector2 v3 = {KurirPos.x + 20, KurirPos.y + 20};
-
-				DrawTriangle(v1, v2, v3, BLUE);
-			}
-
-			// GUI Window File Dialog
+			// GUI: Dialog Window
+			//--------------------------------------------------------------------------------
 			GuiWindowFileDialog(&fileDialogState);
+			//--------------------------------------------------------------------------------
+
+			//----------------------------------------------------------------------------------
+
+			// draw our texture to the screen
+			// DrawTexture(wabbit, 50, 50, WHITE);
+			// GuiButton((Rectangle){ 50, 50, 150, 40 }, "Open File");
+
+			// proses...  cek warna pada tiap posisi pixel
+			// Color pixelColor = GetImageColor(map, 60, 60);
+			// printf("Kode merah %d\n", pixelColor.r);
+			// printf("Kode hijau %d\n", pixelColor.g);
+			// printf("Kode biru %d\n", pixelColor.b);
+
+			// DrawTexture(mapTexture, 50, 50, WHITE);
 		}
 
 		EndDrawing();
 	}
-
+	
 	// cleanup
 	// unload our texture so it can be cleaned up
 	UnloadTexture(map_texture);
