2ce4c6d95f6ea0f33ae4078756e0651f41815ee9
Cahyadi240105 <cahyadiprasetyo659@gmail.com>
2025-03-23 19:37:09 +0700

    Try Again

--- src/main.c
+++ src/main.c
@@ -52,117 +52,62 @@ int main ()
 	SearchAndSetResourceDir("resources");
 
 	// game loop
-	while (!WindowShouldClose()){
-		//  proses drawing
+	// game loop
+	while (!WindowShouldClose())
+	{
 		BeginDrawing();
-	 
-		// Setup the back buffer for drawing (clear color and depth buffers)
 		ClearBackground(BLACK);
 
-
-		if (currentScreen == MENU_STATE){
-			if (GuiButton((Rectangle){screenWidth / 2 - 50, screenHeight / 2 - 20, 100, 40}, "PLAY THE GAME")){
+		if (currentScreen == MENU_STATE)
+		{
+			if (GuiButton((Rectangle){screenWidth / 2 - 50, screenHeight / 2 - 20, 100, 40}, "PLAY THE GAME"))
+			{
 				currentScreen = GAME_STATE;
 			}
-		}else if (currentScreen == GAME_STATE){
-			if (fileDialogState.SelectFilePressed){
-				// Load image file (if supported extension)
-				if (IsFileExtension(fileDialogState.fileNameText, ".png")){
-					// gabung direktori dan nama file
-					strcpy(namaFileGambar, TextFormat("%s" PATH_SEPERATOR "%s", fileDialogState.dirPathText, fileDialogState.fileNameText));
-					
-					// loading message
-					DrawText("Loading image...", screenWidth / 2 - 80, screenHeight / 2, 20, BLUE);
-					EndDrawing();
-
-					// Load image
-					map = LoadImage(namaFileGambar);
-					if (map.data == NULL){
-						printf("Gagal load gambar.\n");
-					}else{
-						UnloadTexture(map_texture);
-						map_texture = LoadTextureFromImage(map);
-					}
-					}
-		
-					fileDialogState.SelectFilePressed = false;
-			}
-
-		
-			DrawTexture(map_texture, GetScreenWidth()/2 - map_texture.width/2, GetScreenHeight()/2 - map_texture.height/2 + 10, WHITE);
-			DrawRectangleLines(GetScreenWidth()/2 - map_texture.width/2, GetScreenHeight()/2 - map_texture.height/2 + 10, map_texture.width, map_texture.height, BLACK);
-		
-			//DrawText(namaFileGambar, 208, GetScreenHeight() - 20, 10, GRAY);
-				
-			if (fileDialogState.windowActive) GuiLock();
-		
-			if (GuiButton((Rectangle){ 20, 20, 140, 30 }, GuiIconText(ICON_FILE_OPEN, "Pilih Map"))){
+		}
+		else if (currentScreen == GAME_STATE)
+		{
+			// Tombol "Pilih Map"
+			if (GuiButton((Rectangle){20, 20, 140, 30}, GuiIconText(ICON_FILE_OPEN, "Pilih Map")))
+			{
 				fileDialogState.windowActive = true;
 			}
-			
-			// jika tombol start ditekan
-			if (GuiButton((Rectangle){screenWidth / 2 - 70, screenHeight / 2 - 15, 140, 30}, "Start Game"))
+
+			// Tombol "Start Game" di samping "Pilih Map"
+			if (GuiButton((Rectangle){180, 20, 140, 30}, "Start Game"))
 			{
 				startgame = true;
-			}
-			
-			else
-			{
-				// Kontrol pergerakan kurir (segitiga) dengan tombol panah
-				if (IsKeyDown(KEY_RIGHT) && KurirPos.x < screenWidth - 20)
-					KurirPos.x += 5; // Gerak kanan
-				if (IsKeyDown(KEY_LEFT) && KurirPos.x > 20)
-					KurirPos.x -= 5; // Gerak kiri
-				if (IsKeyDown(KEY_UP) && KurirPos.y > 20)
-					KurirPos.y -= 5; // Gerak atas
-				if (IsKeyDown(KEY_DOWN) && KurirPos.y < screenHeight - 20)
-					KurirPos.y += 5; // Gerak bawah
 			}
 
 			if (startgame)
 			{
-				// Tampilan awal sebelum game dimulai
-				DrawText("Kurir Smart", screenWidth / 2 - 50, screenHeight / 2 - 50, 20, DARKGRAY);
-			}
-			else
-			{
-				// Menggambar karakter kurir sebagai segitiga siku-siku
-				Vector2 v1 = {KurirPos.x, KurirPos.y - 20};		 // Puncak atas segitiga
-				Vector2 v2 = {KurirPos.x - 20, KurirPos.y + 20}; // Kiri bawah
-				Vector2 v3 = {KurirPos.x + 20, KurirPos.y + 20}; // Kanan bawah
+				// Kontrol pergerakan kurir (segitiga)
+				if (IsKeyDown(KEY_RIGHT) && KurirPos.x < screenWidth - 20)
+					KurirPos.x += 5;
+				if (IsKeyDown(KEY_LEFT) && KurirPos.x > 20)
+					KurirPos.x -= 5;
+				if (IsKeyDown(KEY_UP) && KurirPos.y > 20)
+					KurirPos.y -= 5;
+				if (IsKeyDown(KEY_DOWN) && KurirPos.y < screenHeight - 20)
+					KurirPos.y += 5;
 
-				DrawTriangle(v1, v2, v3, BLUE); // Warna segitiga biru
+				// Menggambar kurir
+				Vector2 v1 = {KurirPos.x, KurirPos.y - 20};
+				Vector2 v2 = {KurirPos.x - 20, KurirPos.y + 20};
+				Vector2 v3 = {KurirPos.x + 20, KurirPos.y + 20};
+				DrawTriangle(v1, v2, v3, BLUE);
 			}
+
 			// GUI File Dialog
 			if (fileDialogState.windowActive)
 				GuiLock();
 
 			GuiWindowFileDialog(&fileDialogState);
 			GuiUnlock();
-			//--------------------------------------------------------------------------------
-		
-			//----------------------------------------------------------------------------------
-		
-		
-			// draw our texture to the screen
-			//DrawTexture(wabbit, 50, 50, WHITE);
-			//GuiButton((Rectangle){ 50, 50, 150, 40 }, "Open File");
-		
-			// proses...  cek warna pada tiap posisi pixel
-			//Color pixelColor = GetImageColor(map, 60, 60);
-			// printf("Kode merah %d\n", pixelColor.r);
-			// printf("Kode hijau %d\n", pixelColor.g);
-			// printf("Kode biru %d\n", pixelColor.b);
-		
-			//DrawTexture(mapTexture, 50, 50, WHITE);
-	
-		
 		}
-
 
 		EndDrawing();
 	}
-	
 
 	// cleanup
 	// unload our texture so it can be cleaned up
